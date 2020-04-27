a38aff8cfbffcea2ca6cbb175cbd16340a387604
Naveen Thomas Varghese <naveen.panickal@gmail.com>
2020-04-27 18:57:15 +0530

    feat: Icons

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
-import {MatButtonModule, MatButtonToggleModule} from "@angular/material";
+import {
+  MatButtonModule, 
+  MatButtonToggleModule, 
+  MatIconModule} from "@angular/material";
 
-const MaterialComponents = [MatButtonModule, MatButtonToggleModule]
+const MaterialComponents = [
+  MatButtonModule, 
+  MatButtonToggleModule,
+  MatIconModule]
 
 @NgModule({
   imports: [MaterialComponents], // Doesn't require declaration as we are only concerned with importing and exporting of components.
